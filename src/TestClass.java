133e1e76231d6eb7623727cd8cae46cdbb0a8e1e
fehimeelif <89203872+fehimeelif@users.noreply.github.com>
2021-08-20 08:42:14 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
     public static void master(String[] args) {
         System.out.println("Git den merhaba!");
+         System.out.println("GitHub dan merhaba!");
     }
 
 
